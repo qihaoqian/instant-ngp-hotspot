f57cecc69b0cb55ea47df8a9c1baae7513caf3d0
Qihao Qian <2363813095@qq.com>
2025-04-14 14:54:36 -0700

    custom configABC

---
+++ config/config.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from typing import Tuple
+from config.config_abc import ConfigABC
+
+@dataclass
+class TrainerConfig(ConfigABC):
+    name : str = "ngp"  # name of the experiment
+    eval_interval : int = 5 # interval for evaluation
+    use_checkpoint : str = "latest" # checkpoint to use for evaluation
+    workspace : str = "workspace" # workspace directory
+    use_tensorboardX : bool = True # use tensorboard for logging
+    max_keep_ckpt : int = 2 # maximum number of checkpoints to keep
+    local_rank : int = 0 # local rank for distributed training
+    world_size : int = 1 # world size for distributed training
+    ema_decay : float = 0.95 # decay for exponential moving average
+    boundary_loss_weight : float = 3e3 # weight for boundary loss
+    eikonal_loss_weight: float = 5e1 # weight for eikonal loss
+    sign_loss_weight: float = 1e2 # weight for sign loss
+    heat_loss_weight: float = 5e1 # weight for heat loss
+    h: float = 1e-4 # step size for finite difference
+    resolution: int = 512 # resolution for output mesh
+
+@dataclass
+class DataConfig(ConfigABC):
+    dataset_path: str = 'data/armadillo.obj'
+    train_size: int = 100
+    valid_size: int = 1
+    num_samples: int = 2**18
+
+@dataclass
+class OptimizerConfig(ConfigABC):
+    type: str = "Adam"               # optimizer type
+    lr: float = 1e-3                 # learning rate
+    weight_decay: float = 1e-6       # weight decay
+    betas: Tuple[float, float] = (0.9, 0.99)  # betas parameter
+    eps: float = 1e-15               # eps parameter
+    
+@dataclass
+class SchedulerConfig(ConfigABC):
+    type: str = "StepLR"  # scheduler type
+    step_size: int = 10   # step size: adjust learning rate every 'step_size' epochs
+    gamma: float = 0.1    # learning rate decay rate
+    
+@dataclass
+class Config(ConfigABC):
+    trainer: TrainerConfig = field(default_factory=TrainerConfig)
+    data: DataConfig = field(default_factory=DataConfig)
+    optimizer: OptimizerConfig = field(default_factory=OptimizerConfig)
+    scheduler: SchedulerConfig = field(default_factory=SchedulerConfig)
+    seed: int = 0
+    test: bool = False
--- main.py
+++ main.py
@@ -1,52 +1,71 @@
 import torch
-import argparse
+from config.config import Config
+from config.config_abc import ConfigABC
 
 from hotspot.utils import *
 
 if __name__ == '__main__':
+    parser = Config.get_argparser()
+    cfg: Config = parser.parse_args()  # python script.py --config config.yaml
+    
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', type=str)
-    parser.add_argument('--test', action='store_true', help="test mode")
-    parser.add_argument('--workspace', type=str, default='workspace')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--lr', type=float, default=1e-3, help="initial learning rate")
-
-    opt = parser.parse_args()
-    print(opt)
-
-    seed_everything(opt.seed)
+    seed_everything(cfg.seed)
 
     from hotspot.netowrk import SDFNetwork
 
     model = SDFNetwork(encoding="hashgrid")
     print(model)
 
-    if opt.test:
-        trainer = Trainer('ngp', model, workspace=opt.workspace, use_checkpoint='best', eval_interval=1, boundary_loss_weight=1e7, eikonal_loss_weight=1e-4, h=1e-3)
-        trainer.save_mesh(os.path.join(opt.workspace, 'results', 'output.ply'), 1024)
+    if cfg.test:
+        trainer = Trainer(
+            name=cfg.trainer.name,
+            model=model,
+            workspace=cfg.trainer.workspace,
+            use_checkpoint=cfg.trainer.use_checkpoint,
+            eval_interval=cfg.trainer.eval_interval,
+            ema_decay=cfg.trainer.ema_decay,
+            use_tensorboardX=cfg.trainer.use_tensorboardX,
+            boundary_loss_weight=cfg.trainer.boundary_loss_weight,
+            eikonal_loss_weight=cfg.trainer.eikonal_loss_weight,
+            sign_loss_weight=cfg.trainer.sign_loss_weight,
+            heat_loss_weight=cfg.trainer.heat_loss_weight,
+            h=cfg.trainer.h
+        )
+                    
+        trainer.save_mesh(os.path.join(cfg.trainer.workspace, 'results', 'output.ply'), 1024)
 
     else:
         from hotspot.dataset import SDFDataset
 
-        train_dataset = SDFDataset(opt.path, size=100, num_samples=2**18)
+        train_dataset = SDFDataset(cfg.data.dataset_path, size=cfg.data.train_size, num_samples=cfg.data.num_samples)
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
 
-        valid_dataset = SDFDataset(opt.path, size=1, num_samples=2**18) # just a dummy
+        valid_dataset = SDFDataset(cfg.data.dataset_path, size=cfg.data.valid_size, num_samples=cfg.data.num_samples) # just a dummy
         valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1)
 
         optimizer = lambda model: torch.optim.Adam([
             {'name': 'encoding', 'params': model.encoder.parameters()},
-            {'name': 'net', 'params': model.backbone.parameters(), 'weight_decay': 1e-6},
-        ], lr=opt.lr, betas=(0.9, 0.99), eps=1e-15)
+            {'name': 'net', 'params': model.backbone.parameters(), 'weight_decay': cfg.optimizer.weight_decay},
+        ], lr=cfg.optimizer.lr, betas=cfg.optimizer.betas, eps=cfg.optimizer.eps)
 
         scheduler = lambda optimizer: optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
-        trainer = Trainer('ngp', model, workspace=opt.workspace, optimizer=optimizer, ema_decay=0.95, 
-                          lr_scheduler=scheduler, use_checkpoint='latest', eval_interval=5, 
-                          boundary_loss_weight=3e3, eikonal_loss_weight=5e1, sign_loss_weight=1e2, heat_loss_weight=5e-1, h=1e-4)
+        trainer = Trainer(
+            name=cfg.trainer.name,
+            model=model,
+            workspace=cfg.trainer.workspace,
+            use_checkpoint=cfg.trainer.use_checkpoint,
+            eval_interval=cfg.trainer.eval_interval,
+            ema_decay=cfg.trainer.ema_decay,
+            use_tensorboardX=cfg.trainer.use_tensorboardX,
+            boundary_loss_weight=cfg.trainer.boundary_loss_weight,
+            eikonal_loss_weight=cfg.trainer.eikonal_loss_weight,
+            sign_loss_weight=cfg.trainer.sign_loss_weight,
+            heat_loss_weight=cfg.trainer.heat_loss_weight,
+            h=cfg.trainer.h
+        )
 
         trainer.train(train_loader, valid_loader, 20)
 
         # also test
-        trainer.save_mesh(os.path.join(opt.workspace, 'results', 'output.ply'), resolution=512)
+        trainer.save_mesh(os.path.join(cfg.trainer.workspace, 'results', 'output.ply'), resolution=512)
