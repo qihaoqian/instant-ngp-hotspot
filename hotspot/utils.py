f8edd01136da04dac7db8b32fb369197f7e39f08
Qihao Qian <2363813095@qq.com>
2025-05-21 19:48:16 -0700

    projectv3 loss stay 0.5

--- hotspot/utils.py
+++ hotspot/utils.py
@@ -386,18 +386,26 @@ class Trainer(object):
             # y_space_det    = space_pred#.detach()
 
             # 2) 随机抽样 N_proj 个点（这里取 2048，或根据实际显存再调小）
-            grad_space = gradients[X_surf.shape[0]:]
-            X_space = X[X_surf.shape[0]:]
-            space_pred = y_pred[X_surf.shape[0]:]
-            N_proj = 2048
-            idx = torch.randperm(X_space.shape[0], device=X.device)[:N_proj]
-            X_space_small     = X_space[idx]
-            grad_small  = grad_space[idx]
-            space_pred_small     = space_pred[idx]
+            N_proj = 256
+            grad_free = gradients[X_surf.shape[0]+X_occ.shape[0]:]
+            X_free = X[X_surf.shape[0]+X_occ.shape[0]:]
+            free_pred = y_pred[X_surf.shape[0]+X_occ.shape[0]:]
             
-            X_proj = X_space_small - grad_small * space_pred_small
+            # idx = torch.randperm(X_space.shape[0], device=X.device)[:N_proj]
+            # X_small     = X_space[idx]
+            # grad_small  = grad_space[idx]
+            # pred_small     = space_pred[idx]
+            
+            idx = torch.randperm(X_free.shape[0], device=X.device)[:N_proj]
+            X_small     = X_free[idx]
+            grad_small  = grad_free[idx].detach()
+            pred_small     = free_pred[idx]
+            
+            X_proj = X_small - grad_small * pred_small
+            # X_proj = X_space - grad_space * space_pred
 
-            dists = torch.full_like(space_pred_small, 3.0)
+
+            # dists = torch.full_like(pred_small, 3.0)
             # for i in range(N_proj):
             #     for j in range(X_surf.shape[0]):
             #         dist = torch.norm(X_proj[i,:] - X_surf[j,:])
@@ -500,6 +508,7 @@ class Trainer(object):
             # Assuming 'writer' is your tensorboard SummaryWriter and is available in the scope.
             self.writer.add_image(f'sdfs_cross_section_xy/epoch{self.epoch}', image, self.epoch, dataformats='HWC')
             buf.close()
+            plt.close()
             # plt.show()
 
         get_sdfs_cross_section(self, bounds_min, bounds_max, resolution, query_func)
